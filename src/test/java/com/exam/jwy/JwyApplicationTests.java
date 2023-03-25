556d71fe68478ced5df5c06a5303cd12eb4859c1
wooyong99 <wulovesk@naver.com>
2023-03-25 11:18:38 +0900

    question 테이블 테스트 수행

--- src/test/java/com/exam/jwy/JwyApplicationTests.java
+++ src/test/java/com/exam/jwy/JwyApplicationTests.java
@@ -1,13 +1,38 @@
 package com.exam.jwy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JwyApplicationTests {
-
+	@Autowired
+	QuestionRepository questionRepository;
 	@Test
-	void contextLoads() {
+	public void testJpa(){
+		Question q1 = new Question();
+		q1.setSubject("제목입니다.");
+		q1.setContent("내용입니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("제목2");
+		q2.setContent("내용2");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
+	}
+	@Test
+	void testJpa2() {
+		List<Question> arr = questionRepository.findAll();
+		Question q = arr.get(0);
+		assertEquals(2,arr.size());
+		assertEquals(q.getSubject(),"제목입니다.");
 	}
 
 }
