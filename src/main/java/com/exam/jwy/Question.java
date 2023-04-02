91d3b1e73f86e51a3b6bbb6af2a2ec11cbc20b62
wooyong99 <wulovesk@naver.com>
2023-04-02 17:10:49 +0900

    질문으로 답변 가져올수 있게 양방향관계 생성

--- src/main/java/com/exam/jwy/Question.java
+++ src/main/java/com/exam/jwy/Question.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -20,5 +21,9 @@ public class Question{
   private String content;
   private LocalDateTime createDate;
   @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-  private List<Answer> answerList;
+  private List<Answer> answerList = new ArrayList<>();
+
+  public void addAnswer(Answer answer){
+    answerList.add(answer);
+  }
 }
--- src/test/java/com/exam/jwy/AnswerRepositoryTest.java
+++ src/test/java/com/exam/jwy/AnswerRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -35,31 +36,38 @@ public class AnswerRepositoryTest {
     QuestionRepositoryTest.createData(questionRepository);
 
     Question q1 = questionRepository.findById(1).get();
+
     Answer a1 = new Answer();
     a1.setContent("1번질문내용");
     a1.setCreateDate(LocalDateTime.now());
     a1.setQuestion(q1);
-
+    q1.addAnswer(a1);
     answerRepository.save(a1);
 
     Question q2 = questionRepository.findById(2).get();
     Answer a2 = new Answer();
     a2.setContent("2번 질문내용");
     a2.setCreateDate(LocalDateTime.now());
-    a2.setQuestion(q2);
+    a2.setQuestion(q1);
+    q1.addAnswer(a2);
 
     answerRepository.save(a2);
   }
   @Test
+  @Transactional
+  @Rollback(false)
   void test1(){
     Answer a = answerRepository.findByContent("1번질문내용");
     assertThat(a.getId()).isEqualTo(1);
   }
   @Transactional
   @Test
+  @Rollback(false)
   void test2(){
-    Question q = questionRepository.findById(2).get();
-    List<Answer> qlist = q.getAnswerList();
-    System.out.println("qlist : "+ qlist.size());
+    Question q = questionRepository.findById(1).get();
+    List<Answer> alist = q.getAnswerList();
+    alist.stream()
+        .map(a -> a.getContent())
+        .forEach(System.out::println);
   }
 }
