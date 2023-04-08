f4531f496619d6fcbd963d0b7251085d0bdfd3bc
wooyong99 <wulovesk@naver.com>
2023-04-09 08:41:16 +0900

    타임리프캐시끄기, 콘솔 설정 추가 및 question리스트 템플릿 작업 추가

--- src/main/java/com/exam/jwy/Question/QuestionController.java
+++ src/main/java/com/exam/jwy/Question/QuestionController.java
@@ -1,12 +1,20 @@
 package com.exam.jwy.Question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor // final 붙은 변수는 생성자 주입을 해준다.
 @Controller
 public class QuestionController {
+  private final QuestionRepository questionRepository;
   @GetMapping("/question/list")
-  public String list(){
-    return "question_list";
+  public String list(Model model){
+    List<Question> questionList = questionRepository.findAll();
+    model.addAttribute("questionList", questionList);
+    return "question_list"; // ResponseBody 어노테이션을 제거하면 templates 파일에 있는 파일을 뷰로 삼는다.
   }
 }
