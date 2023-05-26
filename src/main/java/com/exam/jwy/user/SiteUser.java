a606bc4f56b821457be1ea1aacfcb66373a03311
wooyong99 <wulovesk@naver.com>
2023-05-26 13:02:31 +0900

    SiteUser Entity, Repository 생성

---
+++ src/main/java/com/exam/jwy/user/SiteUser.java
@@ -0,0 +1,21 @@
+package com.exam.jwy.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Column(unique = true)
+    private String email;
+}
---
+++ src/main/java/com/exam/jwy/user/SiteUserRepository.java
@@ -0,0 +1,6 @@
+package com.exam.jwy.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
+}
